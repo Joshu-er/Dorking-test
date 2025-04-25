91f01f169f073dc03e32979d4a3a7446193f0916
Joshu-er <ifenenuwa@gmail.com>
2025-04-25 07:29:29 +0000

    Add files via upload

--- backup.sql
+++ backup.sql
@@ -1,3 +1,11 @@
 -- SQL Dump
--- Password: admin123
-INSERT INTO users VALUES('admin', 'admin123');
+CREATE TABLE users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(100),
+  email VARCHAR(100),
+  password VARCHAR(100)
+);
+
+INSERT INTO users (username, email, password) VALUES
+('admin', 'admin@example.com', 'hashedpassword1'),
+('johndoe', 'john@example.com', 'hashedpassword2');
